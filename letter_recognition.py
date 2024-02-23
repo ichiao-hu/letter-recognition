08fc0c339584ef5b8a9f9435b907aa2c8a957d97
ichiao-hu <160007008+ichiao-hu@users.noreply.github.com>
2024-02-22 22:34:32 -0800

    Add distribution plots

--- letter_recognition.py
+++ letter_recognition.py
@@ -18,3 +18,17 @@ sns.set_style("whitegrid")
 
 # check class imbalance
 y.groupby('lettr')['lettr'].count().plot(kind='bar', figsize=(12, 8))
+
+import matplotlib.pyplot as plt
+
+# draw distribution plots for all features
+
+fig, axes = plt.subplots(nrows=4, ncols=4, figsize=(16, 12))
+axes = axes.flatten()
+
+for i, col in enumerate(X.columns):
+    sns.histplot(X[col], kde=True, ax=axes[i], color='skyblue')
+    axes[i].set_title(f'Distribution of {col}')
+
+plt.tight_layout()
+plt.show()
